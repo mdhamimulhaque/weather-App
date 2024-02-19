17ca2da9b4ecba6a028e7765a6b7c9178e2f735f
md Hamimul Haue <mdhamimulhaque@gmail.com>
2024-02-19 23:10:44 +0600

    weather board component breakdown [done]

--- src/Page.tsx
+++ src/Page.tsx
@@ -1,9 +1,15 @@
 import Header from "./components/Header/Header";
+import WeatherBoard from "./components/Weather/WeatherBoard";
 
 const Page = () => {
   return (
     <>
       <Header />
+      <main>
+        <section>
+          <WeatherBoard />
+        </section>
+      </main>
     </>
   );
 };

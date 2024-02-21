40a5f0772bac5e48c1d216c963cddc7fa04e03b6
md Hamimul Haue <mdhamimulhaque@gmail.com>
2024-02-21 10:40:27 +0600

    api setup & fetch api with custom useWeather hook[done]

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import Page from "./Page";
+import { useWeather } from "./hooks";
+
 const App = () => {
+  const { weatherData } = useWeather();
+  console.log(weatherData);
   return (
-    <div className="bg-body font-[Roboto] text-light bg-[url('../assets/body-bg.png')] bg-no-repeat bg-cover h-screen grid place-items-center">
+    <div>
       <Page />
     </div>
   );
--- src/Page.tsx
+++ src/Page.tsx
@@ -3,14 +3,14 @@ import WeatherBoard from "./components/Weather/WeatherBoard";
 
 const Page = () => {
   return (
-    <>
+    <div className="grid place-items-center h-screen bg-no-repeat bg-cover">
       <Header />
       <main>
         <section>
           <WeatherBoard />
         </section>
       </main>
-    </>
+    </div>
   );
 };
 

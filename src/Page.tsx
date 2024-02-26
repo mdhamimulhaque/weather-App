48410eedc872f1bc7d32f0e717ab918b33f45072
md Hamimul Haue <mdhamimulhaque@gmail.com>
2024-02-26 15:31:10 +0600

    get data from weather context provider,date-time formated & dynamic icon handle [done]

--- src/Page.tsx
+++ src/Page.tsx
@@ -1,16 +1,28 @@
+import { useContext } from "react";
 import Header from "./components/Header/Header";
 import WeatherBoard from "./components/Weather/WeatherBoard";
+import { WeatherContext } from "./context";
 
 const Page = () => {
+  const { loading } = useContext(WeatherContext);
+
   return (
-    <div className="grid place-items-center h-screen bg-no-repeat bg-cover">
-      <Header />
-      <main>
-        <section>
-          <WeatherBoard />
-        </section>
-      </main>
-    </div>
+    <>
+      {loading.state ? (
+        <div className="flex bg-gray-200 rounded-md w-96 p-8 mt-14 mx-auto">
+          <p className="text-center text-3xl text-black">{loading.message}</p>
+        </div>
+      ) : (
+        <div className="grid place-items-center h-screen bg-no-repeat bg-cover">
+          <Header />
+          <main>
+            <section>
+              <WeatherBoard />
+            </section>
+          </main>
+        </div>
+      )}
+    </>
   );
 };
 
